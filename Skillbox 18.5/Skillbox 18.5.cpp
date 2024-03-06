dcec2d47f6d30f0979dd1a1b469db5bf2c296bc9
Timur Azim <papaas73@gmail.com>
2024-03-06 17:48:52 +0500

    Dz

---
+++ Skillbox 18.5/Skillbox 18.5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Player {
+public:
+    string name;
+    int score;
+
+    Player(string n, int s) : name(n), score(s) {}
+};
+
+bool comparePlayers(const Player& a, const Player& b) {
+    return a.score > b.score; // сравнение по убыванию очков
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int numPlayers;
+    cout << "Сколько игроков вы хотите добавить? ";
+    cin >> numPlayers;
+
+    vector<Player> players;
+    for (int i = 0; i < numPlayers; ++i) {
+        string name;
+        int score;
+        cout << "Введите имя игрока: ";
+        cin >> name;
+        cout << "Введите количество набранных очков: ";
+        cin >> score;
+        Player player(name, score);
+        players.push_back(player);
+    }
+
+    sort(players.begin(), players.end(), comparePlayers);
+
+    cout << "\nОтсортированный список игроков:" << endl;
+    for (const Player& player : players) {
+        cout << "Имя: " << player.name << ", Очки: " << player.score << endl;
+    }
+
+    return 0;
+}
